15f2c6c62d51b5a6100554a5a29ce014aeb60a7e
dariathompson <daria.kambalina@gmail.com>
2020-03-10 11:22:35 +0000

    set up test environment

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,6 +3,12 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('https://makers.tech/');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.google.com/');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.netflix.com/browse');")
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include("https://makers.tech/")
--- spec/features/view_list_spec.rb
+++ spec/features/view_list_spec.rb
@@ -1,6 +1,13 @@
 feature 'viewing a list of bookmarks' do
   scenario 'returns a list of bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks (url) VALUES('https://makers.tech/');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.google.com/');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.netflix.com/browse');")
+
     visit ('/bookmarks')
+
     expect(page).to have_content 'https://makers.tech/'
     expect(page).to have_content 'https://www.google.com/'
     expect(page).to have_content 'https://www.netflix.com/browse'
