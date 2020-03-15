768eccbbb5d9c84b19162920ec2094e3ac8e4626
dariathompson <daria.kambalina@gmail.com>
2020-03-15 16:29:08 +0000

    hashing the password

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,6 @@
 require 'database_helpers'
 require 'user'
+require 'bcrypt'
 
 describe User do
   describe '.create' do
@@ -12,6 +13,12 @@ describe User do
       expect(user.id).to eq persisted_data.first['id']
       expect(user.email).to eq 'test@example.com'
     end
+
+    it 'hashes the password using BCrypt' do
+      expect(BCrypt::Password).to receive(:create).with('password123')
+
+      User.create(email: 'test@example.com', password: 'password123')
+    end
   end
 
   describe '.find' do
@@ -21,6 +28,10 @@ describe User do
   
       expect(result.id).to eq user.id
       expect(result.email).to eq user.email
+    end
+
+    it 'returns nil if there is no ID given' do
+      expect(User.find(id: nil)).to eq nil
     end
   end
 
